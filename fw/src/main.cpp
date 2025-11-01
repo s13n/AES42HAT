015c0b8a437c6caa892feface6053872aa0a9dd7
Stefan Heinzmann <stefan_heinzmann@gmx.de>
2025-11-01 12:43:21 +0100

    Enable clocks

--- fw/src/main.cpp
+++ fw/src/main.cpp
@@ -4,12 +4,14 @@
 #define GPIO_DIR (*(volatile uint32_t*)(GPIO0_BASE + 0x2004))
 #define GPIO_SET (*(volatile uint32_t*)(GPIO0_BASE + 0x2204))
 #define GPIO_CLR (*(volatile uint32_t*)(GPIO0_BASE + 0x2284))
+#define SYSAHBCLKCTRL0 (*(volatile uint32_t*)(0x40048080))
 
 extern "C" void delay() {
-    for (volatile int i = 0; i < 100000; ++i);
+    for (volatile int i = 0; i < 300000; ++i);
 }
 
 int main() {
+    SYSAHBCLKCTRL0 = 0x31FFFEF7;
     GPIO_DIR |= (1 << 7); // Set PIO1_7 as output
     while (1) {
         GPIO_SET = (1 << 7); // LED on
