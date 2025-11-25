54316639f64dfa724b876d0ddcb3d42ee0c7cda6
Stefan Heinzmann <stefan_heinzmann@gmx.de>
2025-11-25 23:09:31 +0100

    Improve Channel object

--- fw/src/channel.cpp
+++ fw/src/channel.cpp
@@ -5,6 +5,9 @@
  * @{
  */
 #include "channel.hpp"
+#include "pint_drv.hpp"
+#include "ftm_drv.hpp"
+#include "SRC4392.hpp"
 #include <string_view>
 
 extern void print(std::string_view);
@@ -13,22 +16,22 @@ constexpr std::byte operator""_y(unsigned long long value) {
     return static_cast<std::byte>(value);
 }
 
-static std::array<std::byte, 52> const srcInitData = {
+static std::array<std::byte, 52> const srcInitData0 = {
     0x3F_y, // Register 01: Power-Down and Reset
     0x00_y, // Register 02: Global Interrupt Status (Read-Only)
-    0x31_y, // Register 03: Port A Control Register 1
-    0x00_y, // Register 04: Port A Control Register 2
+    0x39_y, // Register 03: Port A Control Register 1
+    0x03_y, // Register 04: Port A Control Register 2
     0x31_y, // Register 05: Port B Control Register 1
     0x00_y, // Register 06: Port B Control Register 2
-    0x00_y, // Register 07: Transmitter Control Register 1
-    0x10_y, // Register 08: Transmitter Control Register 2
-    0x00_y, // Register 09: Transmitter Control Register 3
+    0x7D_y, // Register 07: Transmitter Control Register 1
+    0x20_y, // Register 08: Transmitter Control Register 2
+    0x01_y, // Register 09: Transmitter Control Register 3
     0x00_y, // Register 0A: SRC and DIT Status (Read-Only)
     0x00_y, // Register 0B: SRC and DIT Interrupt Mask Register
     0x00_y, // Register 0C: SRC and DIT Interrupt Mode Register
     0x08_y, // Register 0D: Receiver Control Register 1
     0x00_y, // Register 0E: Receiver Control Register 2
-    0x12_y, // Register 0F: Receiver PLL1 Configuration Register 1
+    0x22_y, // Register 0F: Receiver PLL1 Configuration Register 1
     0x00_y, // Register 10: Receiver PLL1 Configuration Register 2
     0x00_y, // Register 11: Receiver PLL1 Configuration Register 3
     0x00_y, // Register 12: Non-PCM Audio Detection Status Register (Read-Only)
@@ -37,7 +40,61 @@ static std::array<std::byte, 52> const srcInitData = {
     0x00_y, // Register 15: Receiver Status Register 3 (Read-Only)
     0x01_y, // Register 16: Receiver Interrupt Mask Register 1
     0x00_y, // Register 17: Receiver Interrupt Mask Register 2
-    0x00_y, // Register 18: Receiver Interrupt Mode Register 1
+    0x02_y, // Register 18: Receiver Interrupt Mode Register 1
+    0x00_y, // Register 19: Receiver Interrupt Mode Register 2
+    0x00_y, // Register 1A: Receiver Interrupt Mode Register 3
+    0x01_y, // Register 1B: General-Purpose Output 1 (GPO1) Control Register
+    0x00_y, // Register 1C: General-Purpose Output 2 (GPO2) Control Register
+    0x0E_y, // Register 1D: General-Purpose Output 3 (GPO3) Control Register
+    0x09_y, // Register 1E: General-Purpose Output 4 (GPO4) Control Register
+    0x00_y, // Register 1F: Q-Channel Sub-Code Data Register 1 (Read-Only), Bits[7:0], Control and Address
+    0x00_y, // Register 20: Q-Channel Sub-Code Data Register 2 (Read-Only), Bits[15:8], Track
+    0x00_y, // Register 21: Q-Channel Sub-Code Data Register 3 (Read-Only), Bits[23:16], Index
+    0x00_y, // Register 22: Q-Channel Sub-Code Data Register 4 (Read-Only), Bits[31:24], Minutes
+    0x00_y, // Register 23: Q-Channel Sub-Code Data Register 5 (Read-Only), Bits[39:32], Seconds
+    0x00_y, // Register 24: Q-Channel Sub-Code Data Register 6 (Read-Only), Bits[47:40], Frame
+    0x00_y, // Register 25: Q-Channel Sub-Code Data Register 7 (Read-Only), Bits[55:48], Zero
+    0x00_y, // Register 26: Q-Channel Sub-Code Data Register 8 (Read-Only), Bits[63:56], AMIN
+    0x00_y, // Register 27: Q-Channel Sub-Code Data Register 9 (Read-Only), Bits[71:64], ASEC
+    0x00_y, // Register 28: Q-Channel Sub-Code Data Register 10 (Read-Only), Bits[79:72], AFRAME
+    0x00_y, // Register 29: Burst Preamble PC High-Byte Status Register (Read-Only)
+    0x00_y, // Register 2A: Burst Preamble PC Low-Byte Status Register (Read-Only)
+    0x00_y, // Register 2B: Burst Preamble PD High-Byte Status Register (Read-Only)
+    0x00_y, // Register 2C: Burst Preamble PD Low-Byte Status Register (Read-Only)
+    0x02_y, // Register 2D: SRC Control Register 1
+    0x00_y, // Register 2E: SRC Control Register 2
+    0x00_y, // Register 2F: SRC Control Register 3
+    0x00_y, // Register 30: SRC Control Register 4
+    0x00_y, // Register 31: SRC Control Register 5
+    0x00_y, // Register 32: SRC Ratio Readback Register (Read-Only)
+    0x00_y, // Register 33: SRC Ratio Readback Register (Read-Only)
+};
+
+static std::array<std::byte, 52> const srcInitData = {
+    0x3F_y, // Register 01: Power-Down and Reset
+    0x00_y, // Register 02: Global Interrupt Status (Read-Only)
+    0x31_y, // Register 03: Port A Control Register 1
+    0x00_y, // Register 04: Port A Control Register 2
+    0x31_y, // Register 05: Port B Control Register 1
+    0x00_y, // Register 06: Port B Control Register 2
+    0x79_y, // Register 07: Transmitter Control Register 1
+    0x20_y, // Register 08: Transmitter Control Register 2
+    0x01_y, // Register 09: Transmitter Control Register 3
+    0x00_y, // Register 0A: SRC and DIT Status (Read-Only)
+    0x00_y, // Register 0B: SRC and DIT Interrupt Mask Register
+    0x00_y, // Register 0C: SRC and DIT Interrupt Mode Register
+    0x08_y, // Register 0D: Receiver Control Register 1
+    0x00_y, // Register 0E: Receiver Control Register 2
+    0x22_y, // Register 0F: Receiver PLL1 Configuration Register 1
+    0x00_y, // Register 10: Receiver PLL1 Configuration Register 2
+    0x00_y, // Register 11: Receiver PLL1 Configuration Register 3
+    0x00_y, // Register 12: Non-PCM Audio Detection Status Register (Read-Only)
+    0x00_y, // Register 13: Receiver Status Register 1 (Read-Only)
+    0x00_y, // Register 14: Receiver Status Register 2 (Read-Only)
+    0x00_y, // Register 15: Receiver Status Register 3 (Read-Only)
+    0x01_y, // Register 16: Receiver Interrupt Mask Register 1
+    0x00_y, // Register 17: Receiver Interrupt Mask Register 2
+    0x02_y, // Register 18: Receiver Interrupt Mode Register 1
     0x00_y, // Register 19: Receiver Interrupt Mode Register 2
     0x00_y, // Register 1A: Receiver Interrupt Mode Register 3
     0x01_y, // Register 1B: General-Purpose Output 1 (GPO1) Control Register
@@ -74,37 +131,6 @@ static src4392::Integration const i_src4392[] = {
     { .addr = 3, .cpm = 0, .src_present=1 }};
 
 
-static constexpr std::byte *getPtr(src4392::Src4392 &src, uint8_t addr) {
-    if (addr == 0x7F)
-        return &src.page_;
-    switch (static_cast<uint8_t>(src.page_) & 0x03) {
-    case 0:
-        if (addr == 0)
-            return nullptr;
-        if (addr <= 0x33)
-            return &src.regs_[addr - 1];
-        return nullptr;
-    case 1:
-        if (addr <= 0x2F)
-            return &src.rxcs_[addr];
-        if (addr < 0x40)
-            return nullptr;
-        if (addr <= 0x6F)
-            return &src.rxu_[addr-0x40];
-        return nullptr;
-    case 2:
-        if (addr <= 0x2F)
-            return &src.txcs_[addr];
-        if (addr < 0x40)
-            return nullptr;
-        if (addr <= 0x6F)
-            return &src.txu_[addr-0x40];
-        return nullptr;
-    default:
-        return nullptr;
-    }
-}
-
 bool Channel::select(uint8_t tgt) {
     print("S");
     if ((tgt >> 1) != myaddr_)
@@ -123,7 +149,7 @@ void Channel::deselect() {
 
 uint8_t Channel::getTxByte() {
     print("T");
-    std::byte *ptr = getPtr(src_, addr_ & 0x7F);
+    std::byte *ptr = src_.getPtr(addr_ & 0x7F, page_);
     if (bool inc = addr_ & 0x80)
         addr_ = (addr_ + 1) | 0x80;
     return ptr ? uint8_t(*ptr) : 0;
@@ -136,28 +162,62 @@ void Channel::putRxByte(uint8_t val) {
         expectReg_ = false;
         return;
     }
-    std::byte *ptr = getPtr(src_, addr_ & 0x7F);
+    std::byte *ptr = src_.getPtr(addr_ & 0x7F, page_);
     if (bool inc = addr_ & 0x80)
         addr_ = (addr_ + 1) | 0x80;
     if (ptr) {
         *ptr = std::byte(val);
-        updateSrcPage_ |= 1U << uint8_t(src_.page_);
+        updateSrcPage_ |= 1U << uint8_t(page_);
     }
+}
+
+void Channel::isr() {
+    uint16_t ts1 = ftm_.getCount();
+    uint16_t capt = ftm_.getCapture(2 + (myaddr_ & 0x03));
+    uint16_t ref = ftm_.getCapture(0);
+    uint16_t ts2 = ftm_.getCount();
+    delta_ = capt - ref;
+    pint_.disable(irq_);
+    post();
+}
+
+void Channel::act() {
+    src_.readRxStatus(spi_);
+/*
+    src_.switchPage(spi_, 0x01);
+    updatesCS_ |= src_.readCS(spi_);
+    updatesU_ |= src_.readU(spi_);
+    src_.switchPage(spi_, 0x00);
+*/
+    char buf[3] = { char(myaddr_ - 0x40), '\n' };
+    print(buf);
+
+    pint_.enable(irq_);
 }
 
 void Channel::updateSrcCtrl() {
     src_.writeRegs(spi_);
-    char buf[3] = { char(myaddr_ - 0x40), '\n' };
-    print(buf);
 }
 
-Channel::Channel(unsigned ch, lpc865::Spi &spi)
+void Channel::handleTxBlock() {
+    src_.readTxStatus(spi_);
+}
+
+Channel::Channel(lpc865::Spi &spi, lpc865::Ftm &ftm, lpc865::Pint &pint, uint8_t ch, uint8_t irq)
     : myaddr_{uint8_t(0x70 + ch)}
     , addr_{0}
     , expectReg_{false}
+    , page_{0}
     , updateSrcPage_{0}
+    , irq_{irq}
+    , updatesRegs_{}
+    , updatesCS_{}
+    , updatesU_{}
     , spi_{spi}
+    , ftm_{ftm}
+    , pint_{pint}
     , src_{i_src4392[ch]}
 {
-    src_.updateRegs(srcInitData);
+    src_.updateRegs(ch == 0 ? srcInitData0 : srcInitData);
+    pint_.attach(irq_, *this);
 }    
--- fw/src/channel.hpp
+++ fw/src/channel.hpp
@@ -9,28 +9,65 @@
 #pragma once
 
 #include "i2c_tgt_drv.hpp"
+#include "nvic_drv.hpp"
+#include "handler.hpp"
 #include "src4392_drv.hpp"
 
 namespace lpc865 {
+    class Ftm;
+    class Pint;
     class Spi;
 }
 
-class Channel : public lpc865::I2cTarget::Callback {
+/** Object representing an AES42 channel.
+ * 
+ * The channel object represents an SRC4392 with its associated circuitry.
+ * It processes the interrupt coming from the interrupt pin of the SRC4392,
+ * which is received as a pin interrupt, but also triggers a capture event
+ * on FTM0. The capture event doesn't cause an interrupt, even though it
+ * could, because pin interrupts are more direct and efficient.
+ * 
+ * In normal operation, the interrupt is set up to fire once for each block
+ * received on the DIR of the channel. This signals that
+ * - A phase measurement was taken by FTM0
+ * - A new block of channel status and user data has been received
+ * 
+ * The channel is also attached to the I2C target interface, so that the
+ * host can set and get register settings of the SRC4392. The host has
+ * the impression of talking directly to an SRC4392 in this way.
+ */
+class Channel : public lpc865::I2cTarget::Callback, public arm::Interrupt, public Handler {
 public:
     bool select(uint8_t) override;
     void deselect() override;
     uint8_t getTxByte() override;
     void putRxByte(uint8_t) override;
 
+    void isr() override;
+
+    /** Handles the receive side block event. */
+    void act() override;
+
     void updateSrcCtrl();
-    
-    Channel(unsigned ch, lpc865::Spi &spi);
+
+    /** Handles the transmit side block event. */
+    void handleTxBlock();
+
+    Channel(lpc865::Spi &spi, lpc865::Ftm &ftm, lpc865::Pint &pint, uint8_t ch, uint8_t irq);
 
 private:
     uint8_t myaddr_;        //!< I2C target address of this channel
     uint8_t addr_;          //!< Current register address byte (MSB = INC bit)
-    bool expectReg_;//!< True when expecting register address byte
+    bool expectReg_;        //!< True when expecting register address byte
+    std::byte page_;        //!< Page in access from the I2C side
     uint8_t updateSrcPage_; //!< Pages in SRC chip that needs updating (1 bit for each page)
-    lpc865::Spi &spi_;
-    src4392::Src4392 src_;
+    uint8_t irq_;
+    int16_t delta_;
+    uint64_t updatesRegs_;
+    uint64_t updatesCS_;
+    uint64_t updatesU_;
+    lpc865::Spi &spi_;      //!< SPI port driver to use for controlling the channel
+    lpc865::Ftm &ftm_;      //!< Timer responsible for phase management
+    lpc865::Pint &pint_;
+    src4392::Src4392 src_;  //!< SRC4392 register set cache
 };
